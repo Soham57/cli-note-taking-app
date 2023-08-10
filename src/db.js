ec540b3cd0dfe5754548e780a1c0589ef20dab65
Soham Malviya <sohammalviya65@gmail.com>
2023-08-10 10:56:31 +0530

    getDB function implemented

--- src/db.js
+++ src/db.js
@@ -1,2 +1,7 @@
 import fs from "node:fs/promises";
 const DB_PATH = new URL("../db.json", import.meta.url).pathname;
+
+export const getDB = async () => {
+  const dbData = await fs.readFile(DB_PATH, "utf-8");
+  return JSON.parse(dbData);
+};
