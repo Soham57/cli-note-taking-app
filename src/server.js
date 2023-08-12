9767541b02bf789372ca5024b1f09a218832fdb4
Soham Malviya <sohammalviya65@gmail.com>
2023-08-12 13:51:58 +0530

    createServer function

--- src/server.js
+++ src/server.js
@@ -22,3 +22,14 @@ const formatNotes = (notes) => {
     })
     .join("\n");
 };
+
+const createServer = (notes) => {
+  return http.createServer(async (req, res) => {
+    const HTML_PATH = new URL("./template.html", import.meta.url).pathname;
+    const template = await fs.readFile(HTML_PATH, "utf-8");
+    const html = interpolate(template, { notes: formatNotes(notes) });
+
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  });
+};
