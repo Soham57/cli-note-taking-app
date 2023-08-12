f2fc2ec4d5eb1af89b43a6119810aebc1135eabe
Soham Malviya <sohammalviya65@gmail.com>
2023-08-12 13:51:13 +0530

    formatNotes implemented

--- src/server.js
+++ src/server.js
@@ -7,3 +7,18 @@ const interpolate = (html, data) => {
     return data[placeholder] || "";
   });
 };
+
+const formatNotes = (notes) => {
+  return notes
+    .map((note) => {
+      return `
+      <div class="note">
+        <p>${note.content}</p>
+        <div class="tags">
+          ${note.tags.map((tag) => `<span class="tag">${tag}</span>`).join("")}
+        </div>
+      </div>
+    `;
+    })
+    .join("\n");
+};
